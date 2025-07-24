4fa00b2815203eae28e5090e7c20c43416dba7d6
Judith Ogbole <judithogbole71@gmail.com>
2025-07-24 23:50:01 +0100

    Simple quiz

---
+++ quiz.js
@@ -0,0 +1,15 @@
+function checkAnswer() {
+    var correctAnswer = "4";
+    var userAnswer = document.querySelector("quiz")
+
+    if (userAnswer.value === correctAnswer) {
+        const feedback = document.getElementById("feedback");
+        feedback.textContent = "Correct! Well done.";
+    }
+    else {
+        const feedback = document.getElementById("feedback");
+        feedback.textContent = "That's incorrect. Try again!";
+    }
+}
+var submitAnswer = document.getElementById("submit-answer");
+button.addEventListener("click", checkAnswer);
